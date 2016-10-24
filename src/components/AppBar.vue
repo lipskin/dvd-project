4f0490405e2e09b4463bdc528e02ff3f2906e279
Xing <yellowstar1992@hotmail.com>
2016-10-24 16:19:58 +0800

    init bar

---
+++ src/components/AppBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="app-bar row middle-xs">
+      <span class="logo col-xs-10">
+        DvD
+      </span>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'app-bar',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.app-bar {
+    height: 65px;
+    padding: 5px 30px;
+    background-color: #00BCD4;
+}
+.logo {
+    color: white;
+    font-size: 30px;
+    font-weight: 300;
+    cursor: pointer;
+}
+.link {
+    color: white;
+    font-size: 24px;
+    font-weight: 400;
+    cursor: pointer;
+}
+</style>
